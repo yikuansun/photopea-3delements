d30f7bd4b8c16b5175abcef413f89c59c57cbf8e
Yikuan Sun <yikuansun2015@gmail.com>
2021-07-02 10:51:34 -0400

    Use model-viewer

--- code.js
+++ code.js
@@ -1,16 +1,7 @@
-var clara = claraplayer(document.querySelector("#clara-embed"));
-
-clara.on("loaded", function() {
-    console.log("Clara player is loaded and ready");
-});
-
-// Fetch and initialize the sceneId
-clara.sceneIO.fetchAndUse("790976e3-f99c-4f34-b475-f83fa14693b8");
-clara.player.hideTool("fullscreen");
+var modelViewer = document.querySelector("model-viewer");
 
 function getURI() {
-    var canvas = clara.player.getCanvasElement();
-    return canvas.toDataURL();
+    return modelViewer.toDataURL();
 }
 
 function addToDocument() {
