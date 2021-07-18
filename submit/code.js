85e46d2ba188e46f46e0f4430f026c40d9b67e85
yikuansun <yikuansun2015@gmail.com>
2021-07-17 22:49:23 -0400

    Revert "Delete the stuff"
    This reverts commit 720b0971ba86104414470a26fcef2c89ff3696ed.

---
+++ submit/code.js
@@ -0,0 +1,51 @@
+var thumbURL;
+document.querySelector("input[name=thumbnail]").addEventListener("change", function() {
+    var file = this.files[0];
+    var fileReader = new FileReader();
+    fileReader.onloadend = function(e) {
+        thumbURL = e.target.result;
+    };
+    fileReader.readAsDataURL(file);
+});
+
+var fileURL;
+document.querySelector("input[name=model]").addEventListener("change", function() {
+    var file = this.files[0];
+    var fileReader = new FileReader();
+    fileReader.onloadend = function(e) {
+        fileURL = e.target.result;
+    };
+    fileReader.readAsDataURL(file);
+});
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    fetch(event.target.action, {
+        method: form.method,
+        body: JSON.stringify({
+            _replyto: document.querySelector("input[name=email]").value,
+            data: JSON.stringify({
+                name: document.querySelector("input[name=modelname]").value,
+                author: document.querySelector("input[name=authorname]").value,
+                thumbnail: thumbURL,
+                model: fileURL
+            })
+        }),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(function(response) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset();
+        document.body.style.cursor = "";
+    }).catch(function(error) {
+        status.innerHTML = "Oops! There was a problem submitting your form";
+        document.body.style.cursor = "";
+    });
+    document.body.style.cursor = "wait";
+}
+
+form.addEventListener("submit", handleSubmit);
