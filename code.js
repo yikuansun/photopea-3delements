bb50cb9b995ba03c74df2578d5d41382817e73ac
yikuansun <yikuansun2015@gmail.com>
2021-08-08 20:17:07 -0400

    Use 3DB

--- code.js
+++ code.js
@@ -60,7 +60,7 @@ async function fetchHTTP(url) {
 }
 
 async function getLibraryData() {
-    var library2 = JSON.parse(await fetchHTTP("data/models.json"));
+    var library2 = JSON.parse(await fetchHTTP("https://yikuansun.github.io/3db/models.json"));
     var out = [];
     for (var model of library2) {
         var data = {};
