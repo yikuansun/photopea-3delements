23556c1b9af3a904648b26b4d023f6fa3c33df14
yikuansun <yikuansun2015@gmail.com>
2021-08-26 17:40:41 -0400

    Pick model

--- submit/code.js
+++ submit/code.js
@@ -1,1 +1,13 @@
 const client = filestack.init("ABNI3B81LSv6GvcqZyIOcz");
+
+function pickModel() {
+    var picker = client.picker({
+        maxFiles: 1,
+        minFiles: 1,
+        onUploadDone: function(e) {
+            console.log(e);
+            document.querySelector("input[name=model]").value = e.filesUploaded[0].url;
+        }
+    });
+    picker.open();
+}
